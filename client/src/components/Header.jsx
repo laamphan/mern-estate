472d492f9e75cf829768c61001e8d23b8cf6dbcd
laamphan <41721718+laamphan@users.noreply.github.com>
2024-06-19 14:00:24 +0700

    Update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,11 +8,12 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
-        <Link to="Home"> </Link>
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
-          <span className="text-slate-500">MERN</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+            <span className="text-slate-500">MERN</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -22,7 +23,7 @@ export default function Header() {
           <FaSearch className="text-slate-600"></FaSearch>
         </form>
         <ul className="flex gap-4">
-          <Link to="/home">
+          <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
@@ -33,19 +34,21 @@ export default function Header() {
             </li>
           </Link>
 
-          <Link to="/sign-in">
-            {currentUser ? (
+          {currentUser ? (
+            <Link to="/profile">
               <img
                 className="rounded-full h-7 w-7 object-cover"
                 src={currentUser.avatar}
                 alt=""
               />
-            ) : (
+            </Link>
+          ) : (
+            <Link to="/sign-in">
               <li className="sm:inline text-slate-700 hover:underline">
                 Sign In
               </li>
-            )}{' '}
-          </Link>
+            </Link>
+          )}
         </ul>
       </div>
     </header>

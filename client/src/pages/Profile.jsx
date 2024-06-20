5752362209275c1c237a45b81b5210fae8a650dc
laamphan <41721718+laamphan@users.noreply.github.com>
2024-06-20 15:52:22 +0700

    Add delete listing API

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -146,6 +146,24 @@ export default function Profile() {
     }
   }
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -253,7 +271,12 @@ export default function Profile() {
                 <p className="">{listing.name}</p>
               </Link>
               <div className="flex flex-col">
-                <button className="text-red-700">Delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700">Edit</button>
               </div>
             </div>
